49ddbdd4a0c2279f3ee0f0d09337f6d786065e8c
Alex Vega <axvg@users.noreply.github.com>
2026-04-30 20:28:03 -0500

    add kick animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,15 @@
 extends CharacterBody3D
 
 
-var speed = 3.0
 const JUMP_VELOCITY = 4.5
 
 const mouse_sensitivity = Vector2(.5, .2)
 
+var speed = 3.0
 var walking_speed = 3.0
 var running_speed = 5.0
 var running = false
+var kicking = false
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var animation_player: AnimationPlayer = $visuals/mixamo_base/AnimationPlayer
@@ -25,6 +26,15 @@ func _input(event: InputEvent) -> void:
 		camera_mount.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity.y))
 
 func _physics_process(delta: float) -> void:
+	
+	if !animation_player.is_playing():
+		kicking = false
+	
+	if Input.is_action_just_pressed("kick"):
+		kicking = true
+		animate("kick")
+	
+	
 	if Input.is_action_just_pressed("run"):
 		running = true
 		speed = running_speed
@@ -42,21 +52,24 @@ func _physics_process(delta: float) -> void:
 
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		if running:
-			animate("running")
-		else:
-			animate("walking")
-		
-		visuals.look_at(position * direction)
+		if not kicking:
+			if running:
+				animate("running")
+			else:
+				animate("walking")
+			
+			visuals.look_at(position * direction)
 		
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
 	else:
-		animate("idle")
+		if not kicking:
+			animate("idle")
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
-
-	move_and_slide()
+	
+	if not kicking:
+		move_and_slide()
 
 func animate(animation: String):
 	if animation_player.current_animation != animation:
